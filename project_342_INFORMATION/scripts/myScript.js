39c9ce0446acf67ec7fd4599be1eb96a0f6e99ec
STARLYN REYES <talito06@hotmail.com>
2018-03-15 23:16:07 -0400

    Add files via upload
    CMP 342 project using Html and  CSS. I did this project to show the most expensive cars in the world.  And their history who make them and where.

---
+++ project_342_INFORMATION/scripts/myScript.js
@@ -0,0 +1,9 @@
+
+function message(){
+	console.log('the message was received');
+	alert(" The message was received.");
+}
+
+/*var actionBtn= document.getElementById('btn1');
+
+  actionBtn.addEventListener('click',message, false);*/
